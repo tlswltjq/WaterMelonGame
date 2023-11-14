fecb8e70e76fa6df71d2342115e324f3a19bf742
신지섭 <wltjq1203@icloud.com>
2023-11-14 17:21:18 +0900

    Enum이용한 병합관리까지 완성

--- watermelon/page/GamePA.java
+++ watermelon/page/GamePA.java
@@ -1,21 +1,21 @@
 package watermelon.page;
 
 import processing.core.PApplet;
+
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class GamePA extends PApplet {
 
-    ArrayList<Sphere> spheres;
-    static int wallThickness = 20;
-    Queue<SphereStep> nextSizes;
-    Sphere followingSphere;
-    boolean gameStarted = false;
+    ArrayList<Sphere> spheres;  // ArrayList to store spheres
+    static int wallThickness = 20;  // Thickness of the walls
+    Queue<SphereStep> nextSizes;  // Queue to store next sizes
+    Sphere followingSphere;  // Sphere that follows the mouse pointer
+    boolean gameStarted = false;  // Flag to check if the game has started
 
     public void settings() {
-        pixelDensity(1);
+        pixelDensity(1); // DPI 값을 조절하여 화면이 더 크게 표시되도록 함
         size(600, 750);
     }
 
@@ -23,10 +23,12 @@ public class GamePA extends PApplet {
         spheres = new ArrayList<>();
         nextSizes = new LinkedList<>();
 
+        // Initialize the queue with the next 10 sizes
         for (int i = 0; i < 10; i++) {
             nextSizes.add(SphereStep.values()[(int) (Math.random() * 3)]);
         }
 
+        // Create a sphere to follow the mouse pointer
         followingSphere = new Sphere(this, width / 2f, wallThickness, SphereStep.STEP_1);
     }
 
@@ -34,75 +36,86 @@ public class GamePA extends PApplet {
         background(255);
 
         if (!gameStarted) {
+            // Display "Click to start" message box
             fill(0);
             textSize(32);
             textAlign(CENTER, CENTER);
             text("Click to start", width / 2, height / 2);
         } else {
+            // Update the position of the sphere that follows the mouse pointer
             followingSphere.x = mouseX;
             followingSphere.y = wallThickness;
 
+            // Update the size and step of the followingSphere based on the first size in the queue
+            if (!nextSizes.isEmpty()) {
+                SphereStep firstSize = nextSizes.peek();
+                followingSphere.diameter = firstSize.getSize();
+                followingSphere.step = firstSize.getStep();
+            }
+
+            // Display and update all spheres
             for (int i = spheres.size() - 1; i >= 0; i--) {
                 Sphere s = spheres.get(i);
                 s.display();
                 s.update();
-                s.checkCollision(spheres);
-                s.checkBoundary();
+                s.checkCollision(spheres);  // Check collision with other spheres
+                s.checkBoundary();  // Check collision with boundaries
                 if (s.isMerged) {
-                    spheres.remove(i);
+                    spheres.remove(i);  // Remove merged spheres
                 }
             }
 
-            fill(200);
-            rect(0, height - wallThickness, width, wallThickness);
-            rect(0, 0, wallThickness, height);
-            rect(width - wallThickness, 0, wallThickness, height);
+            // Draw the boundaries
+            fill(200);  // Set fill color to gray for the walls
+            rect(0, height - wallThickness, width, wallThickness);  // Bottom wall
+            rect(0, 0, wallThickness, height);  // Left wall
+            rect(width - wallThickness, 0, wallThickness, height);  // Right wall
 
-            followingSphere.display();
-
+            // Display the current size and step at the top of the screen
             fill(0);
             textSize(16);
-            textAlign(RIGHT, TOP);
-            int index = 0;
-            for (SphereStep step : nextSizes) {
-                text("Next Size " + (index + 1) + ": " + step, width - 10, index * 20 + 10);
-                index++;
-            }
+            textAlign(CENTER, TOP);
+            text("Current Size: " + followingSphere.diameter + " Step: " + followingSphere.step, width / 2, 10);
+
+            // Display and update the sphere that follows the mouse pointer
+            followingSphere.display();
         }
     }
 
     public void mousePressed() {
         if (!gameStarted) {
+            // Start the game on the first click
             gameStarted = true;
         } else {
+            // Get the next size from the queue
             SphereStep nextSize;
             if (!nextSizes.isEmpty()) {
                 nextSize = nextSizes.poll();
             } else {
+                // Use a default size if the queue is empty
                 nextSize = SphereStep.STEP_1;
             }
 
-            // Update the size of the followingSphere to the first size in the queue
-            followingSphere.diameter = nextSizes.peek().getSize();
+            // Update the size of the sphere that follows the mouse pointer
+            followingSphere.diameter = nextSize.getSize();
+            followingSphere.step = nextSize.getStep();
 
+            // Create a new sphere at the top of the screen with the selected size
             Sphere newSphere = new Sphere(this, mouseX, wallThickness, nextSize);
-            newSphere.checkCollision(spheres);
+            newSphere.checkCollision(spheres);  // Check collision with existing spheres
             spheres.add(newSphere);
 
+            // Add a new size to the queue
             nextSizes.add(SphereStep.values()[(int) (Math.random() * 3)]);
             printQueueContents();
         }
     }
 
-
+    // Additional method: Print the contents of the queue to the console
     private void printQueueContents() {
-        int index = 0;
-        for (Iterator<SphereStep> iterator = nextSizes.iterator(); iterator.hasNext(); ) {
-            SphereStep step = iterator.next();
-            if (index >= 1 && index <= 3) {
-                System.out.print(step + " ");
-            }
-            index++;
+        System.out.print("Queue Contents: ");
+        for (SphereStep step : nextSizes) {
+            System.out.print(step + " ");
         }
         System.out.println();
     }
--- watermelon/page/SphereStep.java
+++ watermelon/page/SphereStep.java
@@ -1,25 +1,30 @@
 package watermelon.page;
-
 public enum SphereStep {
-    STEP_1(30),
-    STEP_2(41),
-    STEP_3(56),
-    STEP_4(76),
-    STEP_5(104),
-    STEP_6(141),
-    STEP_7(192),
-    STEP_8(261),
-    STEP_9(355),
-    STEP_10(483),
-    STEP_11(656);
+    STEP_1(30, 1),
+    STEP_2(41, 2),
+    STEP_3(56, 3),
+    STEP_4(76, 4),
+    STEP_5(104, 5),
+    STEP_6(141, 6),
+    STEP_7(192, 7),
+    STEP_8(261, 8),
+    STEP_9(355, 9),
+    STEP_10(483, 10),
+    STEP_11(656, 11);
 
     private final int size;
+    private final int step;
 
-    SphereStep(int size) {
+    SphereStep(int size, int step) {
         this.size = size;
+        this.step = step;
     }
 
     public int getSize() {
         return size;
+    }
+
+    public int getStep() {
+        return step;
     }
 }
