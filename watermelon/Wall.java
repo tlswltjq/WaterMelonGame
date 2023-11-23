1d3bffd3d47ed18f99a891633556d50ad68d08c9
신지섭 <wltjq1203@icloud.com>
2023-11-23 20:32:19 +0900

    세이브 게임오버 기능 점수 기능 잘 작동

--- watermelon/Wall.java
+++ watermelon/Wall.java
@@ -4,86 +4,29 @@ import processing.core.PApplet;
 
 public class Wall {
     PApplet pApplet;
-    float x1, y1; // 첫 번째 클릭 지점
-    float x2, y2; // 두 번째 클릭 지점
-    float thickness = 5;
-    float length = 100;
-    int collisions = 0;
-    int maxCollisions = 2;
-    int color;
+    int startX, startY;
+    boolean isFirstClick = true;
 
-    public Wall(PApplet pApplet, float x1, float y1, float x2, float y2, int color) {
+    public Wall(PApplet pApplet) {
         this.pApplet = pApplet;
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
-        this.color = color;
     }
 
-    public void display() {
-        pApplet.stroke(color);
-        pApplet.strokeWeight(thickness);
-
-        // Draw the line
-        pApplet.line(x1, y1, x2, y2);
-
-        // Draw the end points
-        pApplet.fill(color);
-        pApplet.ellipse(x1, y1, thickness, thickness);
-        pApplet.ellipse(x2, y2, thickness, thickness);
-    }
-
-    public void checkCollision(Sphere sphere) {
-        float d = pApplet.dist(sphere.x, sphere.y, x1, y1);
-
-        // Check collision with the first end point
-        if (d < sphere.diameter / 2) {
-            handleCollision(sphere);
-        }
-
-        d = pApplet.dist(sphere.x, sphere.y, x2, y2);
-
-        // Check collision with the second end point
-        if (d < sphere.diameter / 2) {
-            handleCollision(sphere);
-        }
-
-        // Check collision with the line segment
-        float lineDist = pApplet.dist(x1, y1, x2, y2);
-        float u = ((sphere.x - x1) * (x2 - x1) + (sphere.y - y1) * (y2 - y1)) / (lineDist * lineDist);
-
-        float closestX, closestY;
-
-        if (u >= 0 && u <= 1) {
-            closestX = x1 + u * (x2 - x1);
-            closestY = y1 + u * (y2 - y1);
+    void drawWallBetweenClicks() {
+        if (isFirstClick) {
+            // 첫 번째 클릭 지점 저장
+            startX = pApplet.mouseX;
+            startY = pApplet.mouseY;
+            isFirstClick = false;
         } else {
-            if (u < 0) {
-                closestX = x1;
-                closestY = y1;
-            } else {
-                closestX = x2;
-                closestY = y2;
-            }
-        }
+            // 두 번째 클릭 시 두께 5, 길이 100인 직선 그리기
+            pApplet.strokeWeight(5);
+            pApplet.line(startX, startY, pApplet.mouseX, pApplet.mouseY);
 
-        d = pApplet.dist(sphere.x, sphere.y, closestX, closestY);
-
-        // Check collision with the line segment
-        if (d < sphere.diameter / 2) {
-            handleCollision(sphere);
+            // 첫 번째 클릭 지점 초기화
+            isFirstClick = true;
         }
     }
-
-    private void handleCollision(Sphere sphere) {
-        if (collisions < maxCollisions) {
-            collisions++;
-            // Decrease transparency on collision
-            color = pApplet.color(pApplet.red(color), pApplet.green(color), pApplet.blue(color), 128);
-        } else {
-            // Remove the wall after two collisions
-            color = pApplet.color(pApplet.red(color), pApplet.green(color), pApplet.blue(color), 0);
-        }
+    void mousePressed() {
+        this.drawWallBetweenClicks(); // 객체의 메서드 호출
     }
 }
