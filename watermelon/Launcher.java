b2d6e96c17395cd11f688154586cd91a344eb9bf
신지섭 <wltjq1203@icloud.com>
2023-11-29 08:44:44 +0900

    mac작동확인

--- watermelon/Launcher.java
+++ watermelon/Launcher.java
@@ -5,6 +5,5 @@ import processing.core.PApplet;
 public class Launcher {
     public static void main(String[] args) {
         PApplet.main("watermelon.MainPA");
-//        PApplet.main("watermelon.GamePA");
     }
 }
